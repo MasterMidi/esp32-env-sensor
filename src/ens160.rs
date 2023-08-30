2ab76ef550147d755496d78b002684902d071f94
Michael Andreas Graversen <work@michael-graversen.dk>
2023-08-30 11:02:30 +0000

    added test implementation for sensors

---
+++ src/ens160.rs
@@ -0,0 +1,277 @@
+#![allow(dead_code)]
+
+use core::{
+    convert::TryFrom,
+    ops::{Deref, DerefMut},
+};
+
+use embedded_hal::blocking::i2c::*;
+
+use bitfield::bitfield;
+
+pub const ADDRESS: u8 = 0x53;
+
+// ENS160 Register address
+// This 2-byte register contains the part number in little endian of the ENS160.
+const ENS160_PART_ID_REG: u8 = 0x00;
+// This 1-byte register sets the Operating Mode of the ENS160.
+const ENS160_OPMODE_REG: u8 = 0x10;
+// This 1-byte register configures the action of the INTn pin.
+const ENS160_CONFIG_REG: u8 = 0x11;
+// This 1-byte register allows some additional commands to be executed on the ENS160.
+#[allow(dead_code)]
+const ENS160_COMMAND_REG: u8 = 0x12;
+// This 2-byte register allows the host system to write ambient temperature data to ENS160 for compensation.
+const ENS160_TEMP_IN_REG: u8 = 0x13;
+// This 2-byte register allows the host system to write relative humidity data to ENS160 for compensation.
+#[allow(dead_code)]
+const ENS160_RH_IN_REG: u8 = 0x15;
+// This 1-byte register indicates the current STATUS of the ENS160.
+const ENS160_DATA_STATUS_REG: u8 = 0x20;
+// This 1-byte register reports the calculated Air Quality Index according to the UBA.
+const ENS160_DATA_AQI_REG: u8 = 0x21;
+// This 2-byte register reports the calculated TVOC concentration in ppb.
+const ENS160_DATA_TVOC_REG: u8 = 0x22;
+// This 2-byte register reports the calculated equivalent CO2-concentration in ppm, based on the detected VOCs and hydrogen.
+const ENS160_DATA_ECO2_REG: u8 = 0x24;
+// This 2-byte register reports the temperature used in its calculations (taken from TEMP_IN, if supplied).
+const ENS160_DATA_T_REG: u8 = 0x30;
+// This 2-byte register reports the relative humidity used in its calculations (taken from RH_IN if supplied).
+#[allow(dead_code)]
+const ENS160_DATA_RH_REG: u8 = 0x32;
+// This 1-byte register reports the calculated checksum of the previous DATA_ read transaction (of n-bytes).
+#[allow(dead_code)]
+const ENS160_DATA_MISR_REG: u8 = 0x38;
+// This 8-byte register is used by several functions for the Host System to pass data to the ENS160.
+#[allow(dead_code)]
+const ENS160_GPR_WRITE_REG: u8 = 0x40;
+// This 8-byte register is used by several functions for the ENS160 to pass data to the Host System.
+#[allow(dead_code)]
+const ENS160_GPR_READ_REG: u8 = 0x48;
+
+pub struct Ens160<T>
+where
+    T: WriteRead + Read + Write,
+{
+    i2c: T,
+    address: u8,
+}
+
+impl<T> Ens160<T>
+where
+    T: WriteRead + Read + Write,
+{
+    /// Creates a new sensor driver.
+    pub fn new(i2c: T, address: u8) -> Ens160<T> {
+        Self { i2c, address }
+    }
+
+    /// Releases the underlying I2C bus and destroys the driver.
+    pub fn release(self) -> T {
+        self.i2c
+    }
+}
+
+impl<I2C, E> Ens160<I2C>
+where
+    I2C: Read<Error = E> + Write<Error = E> + WriteRead<Error = E>,
+{
+    /// Resets the device.
+    pub fn reset(&mut self) -> Result<(), E> {
+        self.write_register([ENS160_OPMODE_REG, OperationMode::Reset as u8])
+    }
+
+    /// Switches the device to operational mode.
+    ///
+    /// Call this function when you want the device to start taking measurements.
+    pub fn operational(&mut self) -> Result<(), E> {
+        self.write_register([ENS160_OPMODE_REG, OperationMode::Standard as u8])
+    }
+
+    /// Returns the current status of the sensor.
+    pub fn status(&mut self) -> Result<Status, E> {
+        self.read_register::<1>(ENS160_DATA_STATUS_REG)
+            .map(|v| Status(v[0]))
+    }
+
+    /// Returns the current Air Quality Index (AQI) reading from the sensor.
+    ///
+    /// The AQI is calculated based on the current sensor readings.
+    pub fn airquality_index(&mut self) -> Result<AirqualityIndex, E> {
+        self.read_register::<1>(ENS160_DATA_AQI_REG)
+            .map(|v| AirqualityIndex::from(v[0] & 0x07))
+    }
+
+    /// Returns the Total Volatile Organic Compounds (TVOC) measurement from the sensor.
+    ///
+    /// The TVOC level is expressed in parts per billion (ppb) in the range 0-65000.
+    pub fn tvoc(&mut self) -> Result<u16, E> {
+        self.read_register::<2>(ENS160_DATA_TVOC_REG)
+            .map(u16::from_le_bytes)
+    }
+
+    /// Returns the Equivalent Carbon Dioxide (eCO2) measurement from the sensor.
+    ///
+    /// The eCO2 level is expressed in parts per million (ppm) in the range 400-65000.
+    pub fn eco2(&mut self) -> Result<ECo2, E> {
+        self.read_register::<2>(ENS160_DATA_ECO2_REG)
+            .map(u16::from_le_bytes)
+            .map(ECo2::from)
+    }
+
+    /// Sets the temperature value used in the device's calculations.
+    ///
+    /// Unit is scaled by 100. For example, a temperature value of 2550 should be used for 25.50 °C.
+    pub fn set_temp(&mut self, ambient_temp: i16) -> Result<(), E> {
+        let temp = ((ambient_temp as i32 + 27315) * 64 / 100) as u16;
+        let temp = temp.to_le_bytes();
+        let tbuffer = [ENS160_TEMP_IN_REG, temp[0], temp[1]];
+        self.write_register(tbuffer)
+    }
+
+    /// Sets the relative humidity value used in the device's calculations.
+    ///
+    /// Unit is scaled by 100. For example, a humidity value of 5025 should be used for 50.25% RH.
+    pub fn set_hum(&mut self, relative_humidity: u16) -> Result<(), E> {
+        let rh = (relative_humidity as u32 * 512 / 100) as u16;
+        let rh = rh.to_le_bytes();
+        let hbuffer = [ENS160_RH_IN_REG, rh[0], rh[1]];
+        self.write_register(hbuffer)
+    }
+
+    fn read_register<const N: usize>(&mut self, register: u8) -> Result<[u8; N], E> {
+        let mut write_buffer = [0u8; 1];
+        write_buffer[0] = register;
+        let mut buffer = [0u8; N];
+        self.i2c
+            .write_read(self.address, &write_buffer, &mut buffer)
+            .ok();
+        Ok(buffer)
+    }
+
+    fn write_register<const N: usize>(&mut self, buffer: [u8; N]) -> Result<(), E> {
+        self.i2c.write(self.address, &buffer)
+    }
+}
+
+/// Operation Mode of the sensor.
+#[repr(u8)]
+enum OperationMode {
+    /// DEEP SLEEP mode (low power standby).
+    Sleep = 0x00,
+    /// IDLE mode (low-power).
+    Idle = 0x01,
+    /// STANDARD Gas Sensing Modes.
+    Standard = 0x02,
+    /// Reset device.
+    Reset = 0xF0,
+}
+
+bitfield! {
+    /// Status of the sensor.
+    pub struct Status(u8);
+    impl Debug;
+    pub bool, running_normally, _: 7;
+    pub bool, error, _: 6;
+    pub into Validity, validity_flag, _: 3,2;
+    pub bool, data_is_ready, _: 1;
+    pub bool, new_data_in_gpr, _: 0;
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Validity {
+    NormalOperation,
+    WarmupPhase,
+    InitStartupPhase,
+    InvalidOutput,
+}
+
+impl From<u8> for Validity {
+    fn from(v: u8) -> Self {
+        match v {
+            0b00 => Self::NormalOperation,
+            0b01 => Self::WarmupPhase,
+            0b10 => Self::InitStartupPhase,
+            0b11 => Self::InvalidOutput,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum AirqualityIndex {
+    Excellent = 1,
+    Good = 2,
+    Moderate = 3,
+    Poor = 4,
+    Unhealthy = 5,
+}
+
+impl From<u8> for AirqualityIndex {
+    fn from(i: u8) -> Self {
+        match i {
+            1 => Self::Excellent,
+            2 => Self::Good,
+            3 => Self::Moderate,
+            4 => Self::Poor,
+            5 => Self::Unhealthy,
+            _ => Self::Unhealthy,
+        }
+    }
+}
+
+pub struct ECo2(u16);
+
+impl From<u16> for ECo2 {
+    fn from(v: u16) -> Self {
+        Self(v)
+    }
+}
+
+impl TryFrom<ECo2> for AirqualityIndex {
+    type Error = AirqualityConvError;
+
+    fn try_from(e: ECo2) -> Result<Self, Self::Error> {
+        let value = e.0;
+        match value {
+            400..=599 => Ok(Self::Excellent),
+            600..=799 => Ok(Self::Good),
+            800..=999 => Ok(Self::Moderate),
+            1000..=1499 => Ok(Self::Poor),
+            1500..=u16::MAX => Ok(Self::Unhealthy),
+            _ => Err(AirqualityConvError(value)),
+        }
+    }
+}
+
+impl Deref for ECo2 {
+    type Target = u16;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for ECo2 {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Debug)]
+pub struct AirqualityConvError(pub(crate) u16);
+
+#[cfg(feature = "std")]
+impl std::fmt::Display for AirqualityConvError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(
+            f,
+            "{} is no valid qirquality value. Values start from 400.",
+            self.0
+        )
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for AirqualityConvError {}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use async_broadcast::{broadcast, Receiver, Sender};
+use core::result::Result::Ok;
 
+use embedded_hal::{blocking::delay, prelude::_embedded_hal_blocking_delay_DelayMs};
+use shared_bus::{self, I2cProxy};
+
+use async_broadcast::{broadcast, Receiver, Sender};
+// use ens160::{AirqualityIndex, Ens160};
+use esp_idf_hal::{i2c::*, peripherals::Peripherals, prelude::*};
 use esp_idf_sys::{self as _, esp, esp_app_desc}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
-use tokio::{self};
+use sht4x::Sht4x;
+use tokio::{self, runtime::Handle, task::block_in_place};
 
-use anyhow::{Ok, Result};
+use esp32_threads_demo::ens160::{AirqualityIndex, Ens160};
+
+use anyhow::Result;
 
 esp_app_desc!();
 
@@ -19,36 +28,160 @@ fn main() -> Result<()> {
     let config = esp_idf_sys::esp_vfs_eventfd_config_t { max_fds: 2 };
     esp! { unsafe { esp_idf_sys::esp_vfs_eventfd_register(&config) } }?;
 
+    info!("Setting up board...");
+    let peripherals = Peripherals::take().unwrap();
 
-    info!("Starting async run loop");
+    info!("Starting async run loop...");
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()?
-        .block_on(async_main())?;
+        .block_on(async_main(peripherals))?;
 
-    info!("Shutting down");
+    info!("Shutting down...");
     Ok(())
 }
 
-async fn async_main() -> Result<()> {
+async fn async_main(peripherals: Peripherals) -> Result<()> {
+    let sda = peripherals.pins.gpio3;
+    let scl = peripherals.pins.gpio2;
+    let i2c = peripherals.i2c0;
+
+    let config = I2cConfig::new().baudrate(100.kHz().into());
+    let driver = I2cDriver::new(i2c, sda, scl, &config)?;
+
+    let bus: &'static _ = shared_bus::new_std!(I2cDriver = driver).unwrap();
+
     let (mut s1, r1) = broadcast(1);
+    let (mut sender_ens160, _) = broadcast(1);
+    let (sender_sht45, receiver_sht45) = broadcast(1);
 
     s1.set_overflow(true);
+    sender_ens160.set_overflow(true);
 
     let t1 = tokio::spawn(sensor(s1));
 
     let t2 = tokio::spawn(sensor2(r1));
 
-    let _ = t1.await?;
-    let _ = t2.await?;
+    // let sht45_handler: tokio::task::JoinHandle<()> =
+    //     tokio::spawn(sht45_sensor(sender_sht45, bus.acquire_i2c()));
 
-    Ok(())
+    let ens160_handler = tokio::spawn(ens160_sensor(
+        sender_ens160,
+        receiver_sht45,
+        bus.acquire_i2c(),
+    ));
+
+    // let _ = t1.await?;
+    // let _ = t2.await?;
+    // sht45_handler.await?;
+    match ens160_handler.await? {
+        Ok(_) => info!("ens160_handler: finished"),
+        Err(e) => warn!("ens160_handler: error: {:?}", e),
+    };
+
+    loop {
+        info!("main: sleeping...");
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    }
+}
+
+type Sht45Reading = (u16, f32);
+type Ens160Reading = (u16, u16, AirqualityIndex);
+
+async fn ens160_sensor(
+    tx: Sender<Ens160Reading>,
+    mut rx: Receiver<Sht45Reading>,
+    driver: I2cProxy<'static, std::sync::Mutex<I2cDriver<'_>>>,
+) -> Result<()> {
+    info!("ens160_sensor: setup...");
+    let mut sensor = tokio::time::timeout(
+        std::time::Duration::from_secs(10),
+        tokio::task::spawn_blocking(|| {
+            let mut sensor = Ens160::new(driver, 0x53);
+            info!("ens160_sensor: reseting...");
+            sensor.reset();
+            info!("ens160_sensor: device reset...");
+            Handle::current().block_on(async {
+                tokio::time::sleep(std::time::Duration::from_millis(250)).await;
+            });
+            info!("ens160_sensor: switch to operational mode...");
+            sensor.operational();
+            Handle::current().block_on(async {
+                tokio::time::sleep(std::time::Duration::from_millis(50)).await;
+            });
+            sensor
+        }),
+    )
+    .await??;
+
+    loop {
+        info!("ens160_sensor: checking sensor status...");
+        if let core::result::Result::Ok(status) = sensor.status() {
+            // Wait for data to be ready
+            if status.data_is_ready() {
+                info!("ens160_sensor: check for correction values...");
+                if let core::result::Result::Ok((hum, temp)) = rx.try_recv() {
+                    sensor.set_hum(hum).ok();
+                    sensor.set_temp((temp * 100_f32) as i16).ok();
+                }
+
+                info!("ens160_sensor: reading measurements...");
+                let tvoc = sensor.tvoc().unwrap();
+                let eco2 = sensor.eco2().unwrap();
+                let aqi = sensor.airquality_index().unwrap();
+                info!("ens160_sensor: Broadcasting...");
+                tx.broadcast((tvoc, *eco2, aqi)).await.ok();
+            }
+        }
+
+        info!("ens160_sensor: sleeping...");
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    }
+}
+
+async fn sht45_sensor(
+    tx: Sender<Sht45Reading>,
+    driver: I2cProxy<'_, std::sync::Mutex<I2cDriver<'_>>>,
+) {
+    info!("sht45_sensor: setup...");
+    let mut delay = CustomDelay {};
+    let mut sensor = Sht4x::new(driver);
+
+    loop {
+        info!("sht45_sensor: taking measurement...");
+        let measurement = sensor.measure(sht4x::Precision::Low, &mut delay);
+
+        info!("sht45_sensor: evaluating measurands...");
+        if let core::result::Result::Ok(val) = measurement {
+            info!("sht45_sensor: Broadcasting measurements...");
+            tx.broadcast((
+                val.humidity_percent().to_num(),
+                val.temperature_celsius().to_num(),
+            ))
+            .await
+            .ok();
+        }
+
+        info!("sht45_sensor: sleeping...");
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    }
+}
+
+struct CustomDelay {}
+
+impl embedded_hal::blocking::delay::DelayMs<u16> for CustomDelay {
+    fn delay_ms(&mut self, ms: u16) {
+        info!("Delay provider: sleeping... {ms}");
+        Handle::current().block_on(async {
+            tokio::time::sleep(std::time::Duration::from_millis(ms as u64)).await;
+        });
+    }
 }
 
 async fn sensor(tx: Sender<String>) -> Result<()> {
     for i in 0..10 {
         info!("Broadcasting: {}", i);
-        tx.broadcast(format!("reading of {i}")).await?;
+        tx.broadcast(format!("{i}")).await?;
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
     Ok(())
@@ -60,17 +193,17 @@ async fn sensor2(mut rx: Receiver<String>) -> Result<()> {
             core::result::Result::Ok(x) => x,
             core::result::Result::Err(e) => match e {
                 async_broadcast::RecvError::Closed => {
-                    info!("Channel closed");
+                    warn!("Channel closed");
                     break;
                 }
                 async_broadcast::RecvError::Overflowed(num) => {
-                    info!("Channel overflow, missed {num} message(s)");
+                    warn!("Channel overflow, missed {num} message(s)");
                     continue;
                 }
             },
         };
         info!("Received: {}", data);
-        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     }
 
     Ok(())
